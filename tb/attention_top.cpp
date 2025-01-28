afc9720f10e551e1f734e137b21bb6d0a8342177
Christoph Berganski <christoph.berganski@gmail.com>
2025-01-28 14:19:14 +0100

    Remove poorly maintained testing infrastructure
    By now it should be easier and cleaner (and also more realistic) to test
    the whole FINN integration of the attention operator, preferably with
    some PyTorch or Brevitas design entry (as this is the behavior we are
    trying to match here anyway).

--- tb/attention_top.cpp
+++
@@ -1,8 +0,0 @@
-// Test configuration
-#include "attention_top.hpp"
-
-// Synthesizeable testbench top of the attention operator
-void attention_top(QStream &q, KStream &k, VStream &v, OStream &out) {
-    // Instantiate the attention operator and connect to the input streams
-    Attention attention; attention(q, k, v, out);
-}
--- tests/tests_main.cpp
+++
@@ -1,4 +0,0 @@
-// Setup Boost Tests and configure this file to generate a main function
-#define BOOST_TEST_DYN_LINK
-#define BOOST_TEST_MAIN  // in only one cpp file
-#include <boost/test/unit_test.hpp>
